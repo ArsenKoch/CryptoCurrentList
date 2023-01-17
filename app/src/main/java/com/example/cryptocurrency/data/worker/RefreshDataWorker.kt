f64f855dc9e17fe1f9366601710bdee1b4d34dbe
ArsenKoch <arsenkocaran@icloud.com>
2023-01-17 19:41:53 +0300

    Create a class RefreshDataWorker.kt

---
+++ app/src/main/java/com/example/cryptocurrency/data/worker/RefreshDataWorker.kt
@@ -0,0 +1,32 @@
+package com.example.cryptocurrency.data.worker
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.cryptocurrency.data.database.AppDatabase
+import com.example.cryptocurrency.data.mapper.CoinMapper
+import com.example.cryptocurrency.data.network.ApiFactory
+import kotlinx.coroutines.delay
+
+class RefreshDataWorker(
+    context: Context,
+    workerParams: WorkerParameters
+) : CoroutineWorker(context, workerParams) {
+
+    private val coinInfoDao = AppDatabase.getInstance(context).coinPriceInfoDao()
+    private val mapper = CoinMapper()
+    private val apiService = ApiFactory.apiService
+
+    override suspend fun doWork(): Result {
+        while (true) {
+            val topCoins = apiService.getTopCoinsInfo(limit = 50)
+            val fromSyms = mapper.mapNameListToString(topCoins)
+            val jsonCont = apiService.getFullPriceList(listOfFromSymbols = fromSyms)
+            val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonCont)
+            val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
+            coinInfoDao.insertPriceList(dbModelList)
+            delay(10000)
+        }
+    }
+
+}
